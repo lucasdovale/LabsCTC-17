51947995eda423acb3156ee4235a666594c56543
Lucas Vale <lucas.vale@nubank.com.br>
2021-09-12 17:25:11 -0300

    Upload arquivos referentes ao Lab 1 (v 1.1)

--- Lab 1/questao3_v2.py
+++ Lab 1/questao3_v2.py
@@ -4,32 +4,6 @@ from matplotlib.patches import Circle
 from mpl_toolkits.mplot3d import Axes3D, art3d
 import numpy as np
 
-class Point:
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
-
-class Plot:
-    def __init__(self, function):
-        x = np.linspace(-5, 5, 50)
-        y = np.linspace(-5, 5, 50)
-
-        X, Y = np.meshgrid(x, y)
-        Z = function(X, Y)
-
-        self.fig = plt.figure()
-        self.ax = self.fig.add_subplot(111, projection='3d')
-        self.ax.plot_surface(X, Y, Z, color = 'k')
-    
-    def add_point(self, point, color):
-        p = Circle((point.x, point.y), 0.2, ec=color, fc="none")
-        self.ax.add_patch(p)
-        art3d.pathpatch_2d_to_3d(p, z=point.z, zdir="z")
-
-    def show(self):
-        plt.show()
-
 def f(x, y):
     f = 4 * np.exp(-x**2 - y**2 + 2 * (x + y - 1))
     f = f + np.exp(-(x + 3)**2 - (y - 3)**2)
@@ -39,43 +13,65 @@ def f(x, y):
 
     return f
 
-my_plot = Plot(f)
+
+class plotar_superficie:
+    def __init__(self, funcao):
+        x = np.linspace(-5, 5, 50)
+        y = np.linspace(-5, 5, 50)
+        X, Y = np.meshgrid(x, y)
+        Z = funcao(X, Y)
+        self.fig = plt.figure()
+        self.ax = self.fig.add_subplot(111, projection='3d')
+        self.ax.plot_surface(X, Y, Z, color = 'k')   
+    def adicionar_ponto(self, point, color):
+        p = Circle((point.x, point.y), 0.2, ec=color, fc="none")
+        self.ax.add_patch(p)
+        art3d.pathpatch_2d_to_3d(p, z=point.z, zdir="z")
+    def mostrar(self):
+        plt.show()
 
 
-def simmulated_annealing(search_space, function, T):
-    scale = np.sqrt(T)
-    start_x = np.random.choice(search_space)
-    start_y = np.random.choice(search_space)
+class ponto:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
 
-    x = start_x * 1
-    y = start_y * 1
-    z = function(x, y)
+
+def simmulated_annealing(espaco, funcao, T):
+    ini_x = np.random.choice(espaco)
+    ini_y = np.random.choice(espaco)
+
+    x = ini_x * 1
+    y = ini_y * 1
+    z = funcao(x, y)
 
     for i in range(10000):
-        # next_x = x + np.random.normal() * scale
-        # next_y = y + np.random.normal() * scale
-        next_x = np.random.uniform(-5, 5)
-        next_y = np.random.uniform(-5, 5)
-        next_z = function(next_x, next_y)
-        if next_x > 5 or next_x < -5 or next_y > 5 or next_y < -5 or np.log(np.random.rand()) * T > (next_z - z):
-            next_x = x
-            next_y = y
-            next_z = z
-        x = next_x
-        y = next_y
-        z = next_z
-        T = 0.99 * T # reducing temperature by 1% each iteration
-        p = Point(x, y, z)
-        my_plot.add_point(p, color='b')
+        prox_x = np.random.uniform(-5, 5)
+        prox_y = np.random.uniform(-5, 5)
+        prox_z = funcao(prox_x, prox_y)
+        if prox_x > 5 or prox_x < -5 or prox_y > 5 or prox_y < -5 or np.log(np.random.rand()) * T > (prox_z - z):
+            prox_x = x
+            prox_y = y
+            prox_z = z
+        x = prox_x
+        y = prox_y
+        z = prox_z
+        T = 0.99 * T # reduzindo temperatura em 1% a cada iteração
+        p = ponto(x, y, z)
+        grafico_f.adicionar_ponto(p, color='b')
         
 
     return x, y, z
 
-space_x = np.linspace(-5, 5, 50)
-space_y = np.linspace(-5, 5, 50)
-x_aux, y_aux, z_aux = simmulated_annealing(space_x, f, T = 100)
+# chamando as funções
+grafico_f = plotar_superficie(f)
+espaco_x = np.linspace(-5, 5, 50)
+espaco_y = np.linspace(-5, 5, 50)
+x_aux, y_aux, z_aux = simmulated_annealing(espaco_x, f, T = 100)
 
-q = Point(x_aux, y_aux, z_aux)
-my_plot.add_point(q, color='r')
+# printando último ponto
+q = ponto(x_aux, y_aux, z_aux)
+grafico_f.adicionar_ponto(q, color='r')
 
-my_plot.show()
+grafico_f.mostrar()
